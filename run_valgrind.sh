5de18e9b16e6c6c190dab12cfd0cbfe78e61d118
Zacharia Crumley <zcrumley@zcrumley.(none)>
2011-06-08 14:31:24 +0200

    Added a script called run_valgrind.sh which runs the program in valgrind, piping all output into less.

---
+++ run_valgrind.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#A script to run the voxel prototype program with the standard parameters.
+
+valgrind --leak-check=full ./voxel_generator -s 128 -g 0.5 --scale-shapes -f ~/temp/output -v -x $@ 2>&1 | less
