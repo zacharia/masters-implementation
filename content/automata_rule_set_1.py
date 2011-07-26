fe12c3129ee3900d5a3ca994b523b60475b0c78f
Zacharia Crumley <zcrumley@zcrumley.(none)>
2011-07-26 14:04:10 +0200

    Did some modifications to the automata_rule_set_1.py and basic.material files while tweaking and experimenting with the detailing stuff.

--- content/automata_rule_set_1.py
+++ content/automata_rule_set_1.py
@@ -3,24 +3,30 @@ neighbourhood_size = 1
 
 def main(voxel, neighbourhood, position, octree_size):
     ret = []
-    if position[1] % 2 == 0:
+    #if position[1] % 2 == 0:
+    #if position[1] > 63:
     #if voxel["aggregate_normal"][1] > 0:
-        #ret.append("position_offset 0 0 5")
-        ret.append("material basic/cylinder_default")
+        #ret.append("position_offset 0 5 0")
+        #ret.append("material basic/cylinder_default")
         #ret.append("normalize_normals")
-    else:
-        ret.append("material basic/cube_default")
+    #else:
+        #ret.append("material basic/textureexample")
         #ret.append("position_offset 0 0 -5")
 
     # import code
     # code.interact(local=locals())
-    # for i in range(-neighbourhood_size, neighbourhood_size + 1):
-    #     for j in range(-neighbourhood_size, neighbourhood_size + 1):
-    #         for k in range(-neighbourhood_size, neighbourhood_size + 1):
-    #             print i,j,k,
-    #             print neighbourhood[i][j][k],
-    #             print " ",
-    #         print "\n"
-    #     print "\n\n"
-    # print "=================================================\n\n"
+
+    ret.append("material basic/vertex_colour_lighting")
+
+    if "ring" in voxel["tags"]:
+        ret.append("colour 1.0 0 0 1")
+    if "spire" in voxel["tags"]:
+        ret.append("colour 0 1 0 1")
+    if "spoke" in voxel["tags"]:
+        ret.append("colour 0 0 1 1")
+    if "tower" in voxel["tags"]:
+        ret.append("colour 1 0 1 0.5")
+    if "centre" in voxel["tags"]:
+        ret.append("colour 0 0 0 0.75")
+    
     return ret
