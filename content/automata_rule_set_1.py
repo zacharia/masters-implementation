0f79f6d22759b280851d85a53e9f4d1f87d8935c
Zacharia Crumley <zcrumley@zcrumley.(none)>
2011-08-17 13:33:14 +0200

    Did some work the detailing rule sets and material file to make them ready for demoing.

--- content/automata_rule_set_1.py
+++ content/automata_rule_set_1.py
@@ -1,7 +1,7 @@
-num_iterations = 1
+num_iterations = 90
 neighbourhood_size = 1
 
-def main(voxel, neighbourhood, position, octree_size):
+def space_station_detail(voxel, neighbourhood, position, octree_size):
     ret = []
     #if position[1] % 2 == 0:
     #if position[1] > 63:
@@ -23,27 +23,108 @@ def main(voxel, neighbourhood, position, octree_size):
         ret.append("material basic/vertex_colour_lighting")
         pass
     elif "spire" in voxel["tags"]:
-        #ret.append("colour 0 1 0 1")
-        if position[1] % 3 == 0:
-            ret.append("material spaceship/metal_wall")
-        else:
-            ret.append("material spaceship/window_ugly")
+        ret.append("colour 0 1 0 1")
+        # if position[1] % 3 == 0:
+        #     ret.append("material spaceship/metal_wall")
+        # else:
+        #     ret.append("material spaceship/window_ugly")
         pass
     elif "spoke" in voxel["tags"]:
-        #ret.append("colour 0 0 1 1")
+        ret.append("colour 0 0 1 1")
         ret.append("material basic/vertex_colour_lighting")
         pass
     elif "tower" in voxel["tags"]:
-        #ret.append("colour 1 0 1 0.5")
+        ret.append("colour 1 0 1 0.5")
         ret.append("material basic/vertex_colour_lighting")
         pass
     elif "centre" in voxel["tags"]:
-        #ret.append("colour 0 0 0 0.75")
+        ret.append("colour 0 0 0 0.75")
         ret.append("material basic/vertex_colour_lighting")
         pass
     else:
-        #ret.append("colour 0 0 0 0.75")
+        ret.append("colour 0 0 0 0.75")
         ret.append("material basic/vertex_colour_lighting")
         pass
     
+    return ret
+
+
+def spiral_detail(voxel, neighbourhood, position, octree_size):
+    ret = []
+
+    ret.append("material basic/vertex_colour_lighting")
+    if position[1] % 10 < 5:
+        ret.append("colour 1 0 0 0")
+    else:
+        ret.append("colour 0 1 0 0")
+
+    return ret
+
+
+def fractal_tree_detail(voxel, neighbourhood, position, octree_size):
+    ret = []
+
+    ret.append("material basic/scrolling_metal_wall")
+    
+    # import code
+    # code.interact(local=locals())
+    # rand_num = random.randint(1,3)
+    # if rand_num == 1:
+    #     ret.append("colour 0 0 1 0")
+    # elif rand_num == 2:
+    #     ret.append("colour 0 1 1 0")
+    # elif rand_num == 3:
+    #     ret.append("colour 0 0 0 0")
+
+    return ret
+
+def colour_from_normals(voxel, neighbourhood, position, octree_size):
+    ret = []
+    ret.append("material basic/vertex_colour_lighting")
+    
+    ret.append("colour from_normal")
+        
+    return ret
+
+def rule90(voxel, neighbourhood, position, octree_size):
+    ret = []
+
+    
+
+    if position[0] == 64 and position[1] == 100:
+        ret.append("on")
+
+    upper_row = ["on" in neighbourhood[0][2][1]["detail_info"], "on" in neighbourhood[1][2][1]["detail_info"], "on" in neighbourhood[2][2][1]["detail_info"]]
+    result = True
+
+    if upper_row[0]:
+        if upper_row[1]:
+            if upper_row[2]:
+                result = False
+            else:
+                result = True
+        else:
+            if upper_row[2]:
+                result = False
+            else:
+                result = True
+    else:
+        if upper_row[1]:
+            if upper_row[2]:
+                result = True
+            else:
+                result = False
+        else:
+            if upper_row[2]:
+                result = True
+            else:
+                result = False
+
+    if result:
+        ret.append("on")
+        
+    if "on" in voxel["detail_info"]:
+        ret.append("material basic/vertex_colour_lighting")
+        ret.append("colour from_normal")
+        
     return ret
