ba64f62299cfc8713825e15c33f90d6a5949bdfb
Zacharia Crumley <zcrumley@tesla.cs.uct.ac.za>
2011-10-17 19:06:04 +0200

    Added some new detailing rules to automata_rule_set_1.py. Added some new materials for the new rules to basic.material. Added a whole bunch of new textures for the new materials to basic.material.

--- content/automata_rule_set_1.py
+++ content/automata_rule_set_1.py
@@ -153,11 +153,14 @@ def enterprise(voxel, neighbourhood, position, octree_size, curr_iteration):
         #         ret.append("material basic/vertex_colour_lighting")
         #         ret.append("colour 0.3 0.1 0.8 0")
 
-        if position[1] % 5 == 0:# and random.randint(1,10) == 1:
-            if abs(voxel["aggregate_normal"][0]) > threshold or abs(voxel["aggregate_normal"][2]) > threshold:
-                ret.append("material basic/vertex_colour_lighting")
-                ret.append("colour 0.5 0.5 0 0")
-
+        num = random.randint(1,43)
+        if num == 1:
+            ret.append("material basic/red")
+        elif num == 2:
+            ret.append("material spaceship/window_ugly")
+        elif num == 3:
+            ret.append("material spaceship/metal_wall")
+        
     return ret
 
 def normal_lightness(voxel, neighbourhood, position, octree_size, curr_iteration):
@@ -169,7 +172,129 @@ def normal_lightness(voxel, neighbourhood, position, octree_size, curr_iteration
 
     return ret
 
-def building_windows(voxel, neighbourhood, position, octree_size, curr_iteration):
+
+def skyscraper(voxel, neighbourhood, position, octree_size, curr_iteration):
+    ret = []
+
+    window_material = "material basic/blue"
+    wall_material = "material brick/concrete"
+
+    if "window_pane" in voxel["tags"]:
+        ret.append(window_material)
+    elif "wall" in voxel["tags"]:
+        ret.append(wall_material)
+    
+    return ret
+
+
+# this works nicely, and isn't tag dependent.
+def camoflauge(voxel, neighbourhood, position, octree_size, curr_iteration):
+    ret = []
+    
+    num = random.randint(1,43)
+    if num == 1:
+        ret.append("material basic/brown")
+    elif num == 2:
+        ret.append("material nature/grass")
+    elif num == 3:
+        ret.append("material basic/dark_green")
+        
+    return ret
+
+
+def c_station_detail(voxel, neighbourhood, position, octree_size, curr_iteration):
+    ret = []
+
+    metal_main = "material metal/diamond_plate"
+    metal_spots = "material metal/hole_plate"
+    windows = "material spaceship/window_ugly"
+
+    if curr_iteration == 1:
+      if random.randint(1,100) == 1:
+          ret.append(metal_spots)
+      else:
+          ret.append(metal_main)
+      
+    elif curr_iteration > 1 and curr_iteration <= 4:
+        
+        been_set = False
+        
+        for i in neighbourhood:
+            for j in i:
+                for k in j:
+                    if metal_spots in k["detail_info"]:
+                        ret.append(metal_spots)
+                        been_set = True
+
+        if not been_set:
+            ret.append(metal_main)
+
+    elif curr_iteration == 5:
+        if abs(voxel["aggregate_normal"][1]) == 0 and position[1] % 3 == 1 and random.randint(1,4) == 1:
+            ret.append(windows)
+        else:
+            ret.append(voxel["detail_info"][0])
+
+    # {'aggregate_tags': ['wing'], 'tags': ['wing'], 'solid': True, 'detail_info': ['material basic/green'], 'aggregate_solid': True, 'aggregate_normal': [-0.7071067690849304, -0.7071067690849304, 0.0]}
+        
+    # if "central_spire" in voxel["tags"]:
+    #     if position[1] % 3 == 1 and random.randint(1,4) == 1:
+    #             ret.append("material basic/blue")
+    #     else:
+    #         if random.randint(1,2) == 1:
+    #             ret.append("material metal/diamond_plate")
+    #         else:
+    #             ret.append("material metal/hole_plate")
+        
+    # if "wing" in voxel["tags"]:
+    #     if random.randint(1,2) == 1:
+    #         ret.append("material metal/diamond_plate")
+    #     else:
+    #         ret.append("material metal/hole_plate")
+
+    # if "wing" in voxel["tags"] and abs(voxel["aggregate_normal"][1]) == 0:
+    #     ret.append("material basic/red")
+
+    # for i in neighbourhood:
+    #     for j in i:
+    #         for k in j:
+    #             if "indent" in k["tags"]:
+    #                 ret.append("material basic/red")
+
+    # import code
+    # code.interact(local=locals())
+        
+    return ret
+
+
+# this is constantly changing method I use for seeing how textures look.
+def testing(voxel, neighbourhood, position, octree_size, curr_iteration):
+    return ["material testing"]
+
+
+# this one makes the entire thing covered in metal and with windows.
+def spaceship_windows(voxel, neighbourhood, position, octree_size, curr_iteration):
+    ret = []
+    
+    if abs(voxel["aggregate_normal"][1]) == 0 and position[1] % 3 == 1 and random.randint(1,4) == 1:
+        ret.append("material basic/blue")
+    else:
+        if random.randint(1,2) == 1:
+            ret.append("material metal/diamond_plate")
+        else:
+            ret.append("material metal/hole_plate")
+
+    return ret
+
+
+# this makes each shape that has the tag "col_<num>" a random shade of grey corresponding to num
+#in the shape grammar, assign the random colours with this code:
+
+# value = random.random()
+# the_tag = "col_%f" % value
+# parent.tags.append(the_tag)
+
+def random_grey(voxel, neighbourhood, position, octree_size, curr_iteration):
     ret = []
 
     ret.append("material basic/vertex_colour_lighting")
@@ -183,4 +308,3 @@ def building_windows(voxel, neighbourhood, position, octree_size, curr_iteration
     ret.append("colour %f %f %f 0" % (value, value, value))
     
     return ret
-            
